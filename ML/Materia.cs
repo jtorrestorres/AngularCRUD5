8d397f8d38f145eb75b0b5c4d7e2d89b69cd8089
Jesús Torres <jtorres@digis01.com>
2022-01-27 19:59:52 -0600

    Add project files.

---
+++ ML/Materia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ML
+{
+    public class Materia
+    {
+        [Key]
+        public int IdMateria { get; set; }
+        [Column(TypeName ="varchar(100")]
+        public string Nombre { get; set; }
+        [Column(TypeName = "varchar(100")]
+        public string Descripcion { get; set; }
+        [Column(TypeName = "byte")]
+        public byte Creditos { get; set; }
+    }
+}
